06fc0e3629c5ef7d1f2449ff0672c5f6d1d8a081
esberhamdan <esber.hmd@gmail.com>
2026-03-05 08:18:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from datetime import datetime
 # ==============================
 # ضع بياناتك هنا
 # ==============================
-TOKEN = "8621477107:AAGPbCgxuqDzmjgw3CQx-TmWJ89H0DPqJUE"
+BOT_TOKEN = "8621477107:AAGPbCgxuqDzmjgw3CQx-TmWJ89H0DPqJUE"
 CHAT_ID = "-1003709871403"
 # ==============================
 
